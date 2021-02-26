5f2ea1ba4b2c6d154e749d7266d4d8ba0357659a
Alisher Sayed <78814074+bumble469@users.noreply.github.com>
2021-02-26 15:01:03 +0530

    Add files via upload

---
+++ time.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+class Time
+{  
+    private:
+    int seconds;
+    int hh,mm,ss;
+    public:
+    void getTime(void);
+    void convertIntoSeconds(void);
+    void displayTime(void);
+};
+
+void Time::getTime (void);
+{
+	public:
+ cout<<"Enter time:" << endl;
+  cout << "Hours? "; cin>>hh;
+   cout << "Minutes? "; cin>>mm;
+ cout<< "Seconds? "; cin>>ss;
+}
+void Time::convertIntoSeconds(void)
+{ 
+seconds = hh*3600 + mm*60 + ss 
+}
+void Time::displaytime(void);
+{
+  cout << "the time is = " <<setw(2)<<setfill('0')<<hh":"
+                           <<setw(2)<<setfill('0')<<mm":"
+                           <<setw(2)<<setfill('0')<<ss endl;
+  cout<<" Time in total seconds:"<< seconds
+}
+
+
+int main()
+{
+Time T;
+T.getTime();
+T.convertIntoSeconds();
+T.displayTime();
+return0;
+}
+
